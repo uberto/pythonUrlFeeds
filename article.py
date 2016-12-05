6726745a259e2e4f3f1eb2d00aaf72f376377596
Uberto Barbini <uberto.gama@gmail.com>
2016-12-05 12:12:00 +0000

    close db

--- article.py
+++ article.py
@@ -1,7 +1,15 @@
+import http
 from collections import namedtuple
 
 class Article(namedtuple('Article', 'title html url')):
 
     def isFetched(self):
         return self.html
+
+    def fetchFromUrl(self):
+        conn = http.client.HTTPSConnection(self.url)
+        conn.request("GET", "/")
+        r1 = conn.getresponse()
+        assert r1.status == 200, "wrong status: " + str(r1.status)
+        self.html = r1.read()
 
